fec6fce289dbd1b3c2fb169a3d2838a5e8bfee99
TriumvariteStudios <98315060+Triumvarites@users.noreply.github.com>
2022-05-01 15:27:34 +0600

    Create Modules.lua

---
+++ Config/Modules.lua
@@ -0,0 +1,8 @@
+local Modules = {
+	CreateString = require(script.Parent.Parent.StringServer.CreateString);
+	DestroyString = require(script.Parent.Parent.StringServer.DestroyString);
+	FindString = require(script.Parent.Parent.StringServer.FindString);
+	PrintString = require(script.Parent.Parent.StringServer.PrintString)
+}
+
+return Modules
